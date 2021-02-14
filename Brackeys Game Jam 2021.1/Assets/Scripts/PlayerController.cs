a9d52e4ebd07ef701171c79db5af3bfd55493348
m <martkools@live.nl>
2021-02-14 13:56:40 +0100

    basic movement implementation

--- Brackeys Game Jam 2021.1/Assets/Scripts/PlayerController.cs
+++ Brackeys Game Jam 2021.1/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody rb;
+    private Vector3 trueInputVector;
+    private Vector3 currentEulerAngle;
+
+    public float RotationSpeed;
+    public float AccelerationSpeed;
+    public float MovementSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
     }
 
+
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        rb.AddForce(trueInputVector * Time.fixedDeltaTime * AccelerationSpeed * 100);
+        float velocityY = rb.velocity.y;
+        Vector3 clampedVelocity = Vector3.ClampMagnitude(rb.velocity, MovementSpeed);
+        clampedVelocity.y = velocityY;
+        rb.velocity = clampedVelocity;
+
+
+    }
+
+    private void Update()
+    {
+        //movement
+        float sidewaysMovement = Input.GetAxis("Horizontal");
+        float forwardMovement = Input.GetAxis("Vertical");
+        trueInputVector = transform.forward * forwardMovement + transform.right * sidewaysMovement;
+
+        ////rotation
+        //if (sidewaysMovement != 0)
+        //    currentEulerAngle += new Vector3(0f, sidewaysMovement * RotationSpeed, 0f);
+        //else
+        //    currentEulerAngle = Vector3.Lerp(currentEulerAngle, Vector3.zero, 0.7f);
+        //currentEulerAngle = Vector3.ClampMagnitude(currentEulerAngle, RotationSpeed);
+        //transform.Rotate(currentEulerAngle);
     }
 }
